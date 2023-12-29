065adcfcb991ca8c67fc4cc37523b9ebbdc340bf
midiareshadi <midia.reshadi@gmail.com>
2023-12-29 15:02:59 +0000

    updating dff

---
+++ dff/model/dff_model.py
@@ -0,0 +1,4 @@
+
+def dff_model(d: int) -> int:
+    """dff model"""
+    return d
--- dff/test/test_dff.py
+++ dff/test/test_dff.py
@@ -4,39 +4,50 @@ from pathlib import Path
 
 import cocotb
 from cocotb.clock import Clock
+from cocotb.triggers import Timer
 from cocotb.runner import get_runner
 from cocotb.triggers import RisingEdge
 from cocotb.types import LogicArray
 
+async def reset_dut(reset_n, duration_ns):
+    reset_n.value = 1
+    await Timer(duration_ns, units="ns")
+    reset_n.value = 0
+    reset_n._log.debug("*** Reset complete ***")
 
 @cocotb.test()
-async def dff_simple_test(dut):
+async def dff_tester_function(dut):
 
     initial = (
         LogicArray("U")
     )
-    assert LogicArray(dut.q.value) == initial
+    assert LogicArray(dut.io_q.value) == initial
+
+    reset_n = dut.reset
+
+    reset_thread = cocotb.start_soon(reset_dut(reset_n, duration_ns=15))
+
     
-    dut.d.value = 0
+    dut.io_d.value = 0
 
+    # clock generation
     clock = Clock(dut.clk, 10, units="ns")
-
     cocotb.start_soon(clock.start(start_high=False))
 
     await RisingEdge(dut.clk)
-    expected_val = 0  
-    for i in range(10):
+    expected_val = 0
+    for i in range(5):
         for j in range(2):
             val = j
-            dut.d.value = val 
+            dut.io_d.value = val 
             await RisingEdge(dut.clk)
-            assert dut.q.value == expected_val, f"output q was incorrect on the {i}th cycle"
+            assert dut.io_q.value == expected_val, f"output q was incorrect on the {i}th cycle"
             expected_val = val  
 
     await RisingEdge(dut.clk)
-    assert dut.q.value == expected_val, "output q was incorrect on the last cycle"
+    assert dut.io_q.value == expected_val, "output q was incorrect on the last cycle"
 
-def test_simple_dff_runner():
+def dff_test_runner():
     hdl_toplevel_lang = os.getenv("HDL_TOPLEVEL_LANG", "verilog")
     sim = os.getenv("SIM", "icarus")
 
@@ -44,7 +55,7 @@ def test_simple_dff_runner():
 
     verilog_sources = []
     
-    verilog_sources = [proj_path / "dff.sv"]
+    verilog_sources = [proj_path / "dff.v"]
 
     runner = get_runner(sim)
     runner.build(
@@ -56,4 +67,4 @@ def test_simple_dff_runner():
     runner.test(hdl_toplevel="dff", test_module="test_dff,")
 
 if __name__ == "__main__":
-    test_simple_dff_runner()
+    dff_test_runner()
